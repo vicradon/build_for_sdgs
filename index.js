d347fc74863978e3bfaaf436e42a1f178d27ddc0
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-04-13 14:34:47 +0100

    Set the logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const js2xmlparser = require('js2xmlparser');
+const path = require('path');
 
 const covid19ImpactEstimator = require('./src/estimator');
 const logger = require('./src/logger');
@@ -26,6 +27,67 @@ app.post('/api/v1/on-covid-19', (req, res) => {
 app.post('/api/v1/on-covid-19/json', (req, res) => {
   res.send(covid19ImpactEstimator(req.body));
 });
+app.get('/api/v1/on-covid-19/json' || '/api/v1/on-covid-19', (req, res) => {
+  if (!req.body === null) {
+    res.send(covid19ImpactEstimator(req.body));
+  } else {
+    res.send(covid19ImpactEstimator({
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    }));
+  }
+});
+app.get('/api/v1/on-covid-19', (req, res) => {
+  if (!req.body === null) {
+    res.send(covid19ImpactEstimator(req.body));
+  } else {
+    res.send(covid19ImpactEstimator({
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    }));
+  }
+});
+app.get('/api/v1/on-covid-19/xml', (req, res) => {
+  if (!req.body === null) {
+    const input = covid19ImpactEstimator(req.body);
+    const response = js2xmlparser.parse('estimate', input);
+    res.send(response);
+  } else {
+    const input = {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    };
+    const response = js2xmlparser.parse('estimate', input);
+    res.send(`${response}`);
+  }
+});
 
 app.post('/api/v1/on-covid-19/xml', (req, res) => {
   const input = covid19ImpactEstimator(req.body);
@@ -33,9 +95,10 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   res.send(response);
 });
 
-app.get('/test', (_, res) => {
-  res.send('<h1>It works bro</h1>');
+app.get('/logs', (_, res) => {
+  res.sendFile(path.join(__dirname, './src', 'logs.txt'));
 });
+
 
 app.listen(process.env.PORT || 7000, () => {
   // eslint-disable-next-line no-console
